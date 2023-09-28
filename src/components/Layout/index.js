2d66c58558157f69b23b1c8bdac8b4a3d245026c
Erich Engler <erich.engler@hotmail.com>
2023-09-28 17:01:42 -0500

    slight adjustment to tag margins

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,12 +7,18 @@ const Layout = () => {
         <div className='App'>
             <Sidebar />
             <div className='page'>
-                <span className='tags top-tags'>&lt;body&gt;</span>
+                <span className='tags top-tags'>
+                    &lt;html&gt;
+                    <br />
+                    &nbsp;&nbsp;
+                    &lt;body&gt;
+                </span>
 
                 <Outlet />
 
                 <span className='tags bottom-tags'>
-                    &lt;body&gt;
+                    &nbsp;&nbsp;
+                    &lt;/body&gt;
                     <br />
                     <span className='bottom-tag-html'>&lt;/html&gt;</span>
                 </span>
