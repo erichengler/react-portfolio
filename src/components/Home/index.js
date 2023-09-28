83cb27e776330e73a031a763024af37e2ce8e1fa
Erich Engler <erich.engler@hotmail.com>
2023-09-28 16:44:53 -0500

    messing around with h1 animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,31 @@
 import { Link } from "react-router-dom";
 import './index.scss';
+import { useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
 
 const Home = () => {
-    
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['E', 'r', 'i', 'c', 'h'];
+    const jobArray = ['F', 'u', 'l', 'l', ' ', 'S', 't', 'a', 'c', 'k', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Greetings traveler,<br /> I'm Erich! <br /></h1>
-                <h2>Full Stack Developer
+                <h1>Greetings traveler,<br /> I'm&nbsp;
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        index={15}
+                    /> 
                     <br />
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        index={15}
+                    />
+                    <br />
+                </h1>
+                <h2>
                     Video Game Enthusiast
                     <br />
                     Science Fiction Lover
