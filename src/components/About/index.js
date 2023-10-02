8d29f5356ba2c8a42801f4352485e9b5fd1c8d1c
Erich Engler <erich.engler@hotmail.com>
2023-10-02 14:52:37 -0500

    copied over h1 animation from home page to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import AnimatedLetters from "../AnimatedLetters";
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover');
+        }, 4000)
+    }, []);
 
     return (
         <div className="container about-page">
             <div className="text-zone">
                 <h1>
                     <AnimatedLetters 
+                        letterClass={letterClass}
                         strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
                         index={15}
                     />
