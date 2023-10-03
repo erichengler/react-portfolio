b3a5a865b066e51ef1023754a0aca40141cf6814
Erich Engler <erich.engler@hotmail.com>
2023-10-03 15:04:00 -0500

    added pacman loader to home and about page - changed some animations slightly

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import AnimatedLetters from "../AnimatedLetters";
+import Loader from "react-loaders";
 
 const About = () => {
 
@@ -12,34 +13,37 @@ const About = () => {
     }, []);
 
     return (
-        <div className="container about-page">
-            <div className="text-zone">
-                <h1>
-                    <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-                        index={15}
-                    />
-                </h1>
-                <p>
-                    I'm an ambitious full-stack developer looking for a role in the
-                    tech industry! I'm looking for a company with a transparent
-                    culture and a challenging work environment where I can grow.
-                </p>
-                <p>
-                    As a man of science, I am open-minded and I try to think logically.
-                    <br />I take pride in my attention to detail and my ability to adapt
-                    to any situation. My peers tell me that I'm easy to work with
-                    and fun to be around.
-                </p>
-                <p>
-                    In my free time, you'll mostly find me playing video games
-                    (especially challenging ones) or spending time with friends and family.
-                    In the world of gaming, its common to get stuck and have to change your strategy.
-                    These are the situations that I look forward to and thrive in.
-                </p>
+        <>
+            <div className="container about-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+                            index={15}
+                        />
+                    </h1>
+                    <p>
+                        I'm an ambitious full-stack developer looking for a role in the
+                        tech industry! I'm looking for a company with a transparent
+                        culture and a challenging work environment where I can grow.
+                    </p>
+                    <p>
+                        As a man of science, I am open-minded and I try to think logically.
+                        <br />I take pride in my attention to detail and my ability to adapt
+                        to any situation. My peers tell me that I'm easy to work with
+                        and fun to be around.
+                    </p>
+                    <p>
+                        In my free time, you'll mostly find me playing video games
+                        (especially challenging ones) or spending time with friends and family.
+                        In the world of gaming, its common to get stuck and have to change your strategy.
+                        These are the situations that I look forward to and thrive in.
+                    </p>
+                </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 import './index.scss';
 import { useEffect, useState } from "react";
 import AnimatedLetters from "../AnimatedLetters";
+import Loader from "react-loaders";
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['E', 'r', 'i', 'c', 'h', '!'];
     const jobArray = [
-        'F', 'u', 'l', 'l', 
-        ' ', 
-        'S', 't', 'a', 'c', 'k', 
-        ' ', 
+        'F', 'u', 'l', 'l',
+        ' ',
+        'S', 't', 'a', 'c', 'k',
+        ' ',
         'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'
     ];
 
@@ -21,37 +22,40 @@ const Home = () => {
     }, []);
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={`${letterClass} _11`}>H</span>
-                    <span className={`${letterClass} _12`}>i</span>
-                    <span className={`${letterClass} _12`}>,</span>
-                    <br />
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'</span>
-                    <span className={`${letterClass} _14`}>m</span>&nbsp;
-                    <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={nameArray}
-                        index={15}
-                    /> 
-                    <br />
-                    <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={jobArray}
-                        index={16}
-                    />
-                    <br />
-                </h1>
-                <h2>
-                    Video Game Enthusiast
-                    <br />
-                    Science Fiction Lover
-                </h2>
-                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={`${letterClass} _11`}>H</span>
+                        <span className={`${letterClass} _12`}>i</span>
+                        <span className={`${letterClass} _12`}>,</span>
+                        <br />
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'</span>
+                        <span className={`${letterClass} _14`}>m</span>&nbsp;
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={nameArray}
+                            index={15}
+                        />
+                        <br />
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={jobArray}
+                            index={16}
+                        />
+                        <br />
+                    </h1>
+                    <h2>
+                        Video Game Enthusiast
+                        <br />
+                        Science Fiction Lover
+                    </h2>
+                    <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+                </div>
             </div>
-        </div>
+            <Loader type='pacman' />
+        </>
     )
 }
 
