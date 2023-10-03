b3462165ab6788a241efc7d949108dbfc9e91665
Erich Engler <erich.engler@hotmail.com>
2023-10-03 16:22:47 -0500

    added h1 to contact page - copied animation from about page

---
+++ src/components/Contact/index.js
@@ -0,0 +1,34 @@
+import Loader from 'react-loaders';
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+
+const Contact = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover');
+        }, 3000)
+    }, []);
+    
+    return (
+        <>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                        letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']}
+                            index={15}
+                        />
+                    </h1>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Contact;
